68450055c294e96d980178d96de9501289f443c6
Jason Bobich <jasonbobich@gmail.com>
2018-09-25 17:38:29 -0600

    Re-configure store to maintain search params in state.

--- client/store/sessions.js
+++ client/store/sessions.js
@@ -2,18 +2,64 @@ import axios from 'axios';
 
 // Intial State
 
-const initialState = ''; // Normally an object; using string here to represent returned SQL query statment.
+const initialState = {
+  params: [
+    {
+      name: '',
+      reducer: '',
+      value: ''
+    }
+  ],
+  results: '' // Would be an object of sessions, but using string to represent returned SQL query statement.
+};
 
 // Action Types
 
+const ADD_PARAM = 'ADD_PARAM';
+const UPDATE_PARAM = 'UPDATE_PARAM';
+const REMOVE_PARAM = 'REMOVE_PARAM';
 const GET_SESSIONS = 'GET_SESSIONS';
 
 // Reducer
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
+    // Add a search parameter set.
+    case ADD_PARAM:
+      return {
+        ...state,
+        params: [...state.params, { name: '', reducer: '', value: '' }]
+      };
+      break;
+
+    // Update a search parameter set.
+    case UPDATE_PARAM:
+      const { index, field, value } = action;
+      const newParams = [...state.params]; // Don't mutuate original state.
+      const newParam = newParams[index];
+
+      newParam[field] = value;
+      newParams[index] = newParam;
+
+      return {
+        ...state,
+        params: newParams
+      };
+      break;
+
+    // Remove a search parameter set.
+    case REMOVE_PARAM:
+      return {
+        ...state,
+        params: state.params.filter((param, index) => index !== action.index)
+      };
+      break;
+
+    // Get sessions (i.e. would be search results from db)
     case GET_SESSIONS:
-      return action.sessions;
+      return { ...state, results: action.sessions };
+      break;
+
     default:
       return state;
   }
@@ -21,10 +67,32 @@ export default function reducer(state = initialState, action) {
 
 // Actions
 
-export function getSessions() {
+export function addParam() {
+  return {
+    type: ADD_PARAM
+  };
+}
+
+export function updateParam(index, field, value) {
+  return {
+    type: UPDATE_PARAM,
+    index,
+    field,
+    value
+  };
+}
+
+export function removeParam(index) {
+  return {
+    type: REMOVE_PARAM,
+    index
+  };
+}
+
+export function getSessions(params) {
   return dispatch => {
     axios
-      .get('/sessions')
+      .get('/sessions', params)
       .then(response => {
         dispatch({
           type: GET_SESSIONS,
