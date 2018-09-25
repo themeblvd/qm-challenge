92a912831132fa62625952bd78b3ce6456e2b23c
Jason Bobich <jasonbobich@gmail.com>
2018-09-25 17:35:05 -0600

    Organize all form options in their own utility file.

---
+++ client/utils/options.js
@@ -0,0 +1,97 @@
+export const names = [
+  {
+    id: 'user_email',
+    label: 'User Email',
+    type: 'string'
+  },
+  {
+    id: 'user_first_name',
+    label: 'User First Name',
+    type: 'string'
+  },
+  {
+    id: 'user_last_name',
+    label: 'User Last Name',
+    type: 'string'
+  },
+  {
+    id: 'screen_width',
+    label: 'Screen Width',
+    type: 'number'
+  },
+  {
+    id: 'screen_height',
+    label: 'Screen Height',
+    type: 'number'
+  },
+  {
+    id: 'visits',
+    label: 'Visits',
+    type: 'number'
+  },
+  {
+    id: 'page_response',
+    label: 'Page Reponse',
+    type: 'string'
+  },
+  {
+    id: 'domain',
+    label: 'Domain',
+    type: 'string'
+  },
+  {
+    id: 'path',
+    label: 'Path',
+    type: 'string'
+  }
+];
+
+export const stringReducers = [
+  {
+    id: 'starts_with',
+    label: 'starts with'
+  },
+  {
+    id: 'not_starts_with',
+    label: 'does not start with'
+  },
+  {
+    id: 'equals',
+    label: 'equals'
+  },
+  {
+    id: 'not_equals',
+    label: 'does not equal'
+  },
+  {
+    id: 'contains',
+    label: 'contains'
+  },
+  {
+    id: 'not_contains',
+    label: 'does not contain'
+  }
+];
+
+export const numberReducers = [
+  {
+    id: 'range',
+    label: 'in range'
+  },
+  {
+    id: 'less',
+    label: 'less than or equal to'
+  },
+  {
+    id: 'equals',
+    label: 'equal to'
+  },
+  {
+    id: 'not_equals',
+    label: 'not equal to'
+  },
+  {
+    id: 'greater',
+    label: 'greater than or equal to'
+  }
+];
