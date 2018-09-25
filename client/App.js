72f353bf843be45352e46b1b244fd9c22b692a33
Jason Bobich <jasonbobich@gmail.com>
2018-09-25 17:39:34 -0600

    Simplify top-level App() and disconnect from store.

--- client/App.js
+++ client/App.js
@@ -1,41 +1,19 @@
 import React, { Component } from 'react';
-import './assets/scss/main.scss'; // Must come before components.
+import './assets/scss/main.scss';
 import { connect } from 'react-redux';
 import { getSessions } from './store/sessions';
+import SearchForm from './components/SearchForm';
 
 /**
  * Top-Level Application.
  */
-class App extends Component {
-  /**
-   * Fetch "sessions" from the database, when the
-   * component mounts.
-   *
-   * Note: In our example app, this will simply just
-   * be a string, representing an SQL query, opposed
-   * to an actual object of retrieved user sessions.
-   */
-  componentDidMount() {
-    this.props.getSessions();
-  }
+const App = () => {
+  return (
+    <div className="app">
+      <h1 className="page-title">Search For Sessions</h1>
+      <SearchForm />
+    </div>
+  );
+};
 
-  /**
-   * Render component.
-   *
-   * @return {Component}
-   */
-  render() {
-    const { sessions } = this.props;
-
-    return (
-      <div className="app">
-        <p>@TODO</p>
-      </div>
-    );
-  }
-}
-
-export default connect(
-  state => ({ sessions: state.sessions }), // Lack of brevity for clarity, could be `({ ...state })`.
-  { getSessions }
-)(App);
+export default App;
