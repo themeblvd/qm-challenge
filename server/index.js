51b5da713c5575fc966860a1767ca62f321da204
Jason Bobich <jasonbobich@gmail.com>
2018-09-26 15:49:55 -0600

    Get rid of body-parser; it's not needed in this project.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 import Express from 'express';
-import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import path from 'path';
 import sessionsRouter from './routes/sessions';
@@ -9,15 +8,11 @@ const port = 8080;
 const isDevMode = process.env.NODE_ENV === 'development' || false;
 const staticPath = isDevMode ? '../client/public' : '../dist/public';
 
-// Apply middleware.
 isDevMode && app.use(morgan('dev'));
-app.use(bodyParser.json());
 
-// Set up end points.
 app.use('/sessions', sessionsRouter);
 app.use('/', Express.static(path.join(__dirname, staticPath)));
 
-// Run server.
 app.listen(port, function() {
   console.log(`The server is now running on port ${port}.`);
 });
